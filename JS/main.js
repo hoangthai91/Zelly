463df93c34afb1b7553ce4ad401876ca1bebdce6
hoangthai91 <hongthaismile@gmail.com>
2018-12-18 13:44:54 +0700

    dsad

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    $(".slide-house").slick({ dots: false, adaptiveHeight: true, autoplaySpeed: 3500, arrows: false, autoplay: true, slidesToShow: 1, slidesToScroll: 1, });
+    $(".pic-achievements , .pic-partner").slick({ dots: false, adaptiveHeight: true, autoplaySpeed: 2000, arrows: false, autoplay: true, slidesToShow: 5, slidesToScroll: 1, });
     $('.search-option').hide();
     $(".main-search").on('click', function() { $('.search-option').animate({ height: 'toggle', }) });
     $("#menu").mmenu({ extensions: ["position-bottom", "fullscreen", "theme-black", "listview-50", "fx-panels-slide-up", "fx-listitems-drop", "border-offset"], navbar: { title: "" }, navbars: [{ height: 2, content: ['<a href="#/" class="fa fa-phone"></a>', '<img src="img/profile-2-w.png" />', '<a href="#/" class="fa fa-envelope"></a>'] }, { content: ["prev", "title"] }] }, {});
@@ -9,6 +11,5 @@ $(document).ready(function() {
     $(".list-slider-areas").slick({ dots: false, adaptiveHeight: true, autoplaySpeed: 2000, arrows: true, autoplay: false, slidesToShow: 3, slidesToScroll: 3, });
     $(".product-slide").slick({ dots: false, adaptiveHeight: true, autoplaySpeed: 2000, arrows: true, autoplay: false, slidesToShow: 4, slidesToScroll: 4, });
     $(".slide-coke").slick({ dots: false, adaptiveHeight: true, autoplaySpeed: 3000, arrows: false, autoplay: true, slidesToShow: 1, slidesToScroll: 1, });
-    $(".slide-house").slick({ dots: false, adaptiveHeight: true, autoplaySpeed: 3500, arrows: false, autoplay: true, slidesToShow: 1, slidesToScroll: 1, });
-    $(".pic-achievements , .pic-partner").slick({ dots: false, adaptiveHeight: true, autoplaySpeed: 2000, arrows: false, autoplay: true, slidesToShow: 5, slidesToScroll: 1, })
+
 });
